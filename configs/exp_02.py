9b683be9490e27f24dafc62202dfb67ebe573881
Mai Anh Vu <maianhuel@gmail.com>
2025-04-29 21:32:08 -0500

    update

--- configs/exp_02.py
+++ configs/exp_02.py
@@ -8,8 +8,8 @@ learning_rate = 1e-4
 
 
 if dataset_name == 'bcss': 
-    datastet_dir ="/project/hnguyen2/mvu9/datasets/processing_datasets/BCSS-WSSS_organized"
+    data_dir ="/project/hnguyen2/mvu9/datasets/processing_datasets/BCSS-WSSS_organized"
 elif dataset_name == 'luad':
-    datastet_dir =  "/project/hnguyen2/mvu9/datasets/processing_datasets/LUAD-HistoSeg_organized"   
+    data_dir =  "/project/hnguyen2/mvu9/datasets/processing_datasets/LUAD-HistoSeg_organized"   
 else:
     raise ValueError("Invalid dataset name. Choose either 'bcss' or 'luad'.")  
