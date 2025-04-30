b6cb59d8b711a927156849958d216137ae27f75c
Mai Anh Vu <maianhuel@gmail.com>
2025-04-30 06:58:34 -0500

    update

--- preprocessing/precompute_vqgan_indices.py
+++ preprocessing/precompute_vqgan_indices.py
@@ -1,21 +1,18 @@
-# precompute_indices.py
-
 import os
-import sys 
-import shutil 
+import sys
+import shutil
 import torch
 from tqdm import tqdm
- 
 
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-print(f"Project root added to sys.path: {PROJECT_ROOT}")  
-sys.path.append(PROJECT_ROOT) 
+print(f"Project root added to sys.path: {PROJECT_ROOT}")
+sys.path.append(PROJECT_ROOT)
 from src.datasets import create_dataloaders
-from src.models import VQGANProcessor 
-from utils import load_config 
+from src.models import VQGANProcessor
+from utils import load_config
 
 def load_vqgan(config, ckpt_path=None, is_gumbel=False):
-    from taming.models.vqgan import VQModel, GumbelVQ 
+    from taming.models.vqgan import VQModel, GumbelVQ
     model_cls = GumbelVQ if is_gumbel else VQModel
     model = model_cls(**config.model.params)
     
@@ -23,7 +20,6 @@ def load_vqgan(config, ckpt_path=None, is_gumbel=False):
         sd = torch.load(ckpt_path, map_location="cpu")["state_dict"]
         model.load_state_dict(sd, strict=False)
     return model.eval()
-
 
 def precompute_indices(data_dir, dataset_name, vqgan_logs_dir, output_dir_root=None, is_gumbel=True, device="cuda"):
     DEVICE = torch.device(device)
@@ -48,50 +44,45 @@ def precompute_indices(data_dir, dataset_name, vqgan_logs_dir, output_dir_root=N
         shutil.rmtree(output_dir_root)  # Remove existing directory and all contents
     os.makedirs(output_dir_root)        # Create a fresh directory 
     
-    for split in ["train", "val", "test"]:
-        print(f"\n Processing split: {split}")
+    # Load dataloaders for all splits
+    train_loader, val_loader, test_loader = create_dataloaders(
+        dataroot=data_dir,
+        dataset=dataset_name,
+        batch_size=16,
+        num_workers=4,
+        stage="stage2"
+    )
+
+    for split, loader in [("train", train_loader), ("val", val_loader), ("test", test_loader)]:
+        print(f"\nProcessing split: {split}")
         
         # Create output directory
         output_dir = os.path.join(output_dir_root, split, "indices")
+        if split != "train":
+            output_dir = os.path.join(output_dir_root, split, "img", "indices")
         if os.path.exists(output_dir):
             shutil.rmtree(output_dir)  # Remove existing directory and all contents
         os.makedirs(output_dir)        # Create a fresh directory 
         
-         
-        # Load appropriate dataloader
-        train_loader, val_loader, _ = create_dataloaders(
-            dataroot=data_dir,
-            dataset=dataset_name,
-            batch_size=16,
-            num_workers=4,
-            stage="stage2"
-        )
-        loader = train_loader if split == "train" else val_loader
-        
         for batch in tqdm(loader, desc=f"Encoding {split}", leave=False):
-        # for batch in loader:
             images = batch["image"].to(DEVICE)
             indices = vqgan_processor(images)  # Shape: [B, H, W] or [B, T]
 
             for i, idx in enumerate(indices):
                 try:
                     image_path = batch["image_path"][i]  # Should be string
-                    # print(i, image_path) 
                     image_name = os.path.splitext(os.path.basename(image_path))[0]
-                    # print(f"Image name: {image_name}")
                     save_path = os.path.join(output_dir, f"{image_name}.pt")
                     torch.save(idx.cpu(), save_path)
-                    # print(f"Saved index: {save_path}")
                 except Exception as e:
                     print(f"Failed to save index for item {i}: {e}")
 
 if __name__ == "__main__":
-    # dataset_name = 'bcss'
-    dataset_name = 'luad'  
+    dataset_name = 'bcss'
+    # dataset_name = 'luad'  
     
     if dataset_name == 'luad': 
         dataroot = "/project/hnguyen2/mvu9/datasets/processing_datasets/LUAD-HistoSeg_organized"   
-    
     elif dataset_name == 'bcss':
         dataroot = "/project/hnguyen2/mvu9/datasets/processing_datasets/BCSS-WSSS_organized"     
     
@@ -101,6 +92,3 @@ if __name__ == "__main__":
         vqgan_logs_dir="/project/hnguyen2/mvu9/folder_04_ma/wsi_efficient_seg/resources/vqgan/logs",
         is_gumbel=True
     )
-
-# TCGA-S3-AA15-DX1_xmin55486_ymin28926_MPP-0.2500+9.png
-# TCGA-S3-AA15-DX1_xmin55486_ymin28926_MPP-0.2500+9.pt 
